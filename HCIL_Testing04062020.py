a10e49b097e2f6b73720398403a04d98723bba66
Aditya Kumar <adkum125@in.ibm.com>
2020-06-17 21:12:09 +0530

    15th launch

--- HCIL_Testing04062020.py
+++ HCIL_Testing04062020.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_auth
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
@@ -12,6 +13,11 @@ import glob
 print('start')
 
 
+VALID_USERNAME_PASSWORD_PAIRS = {
+    'hello': 'world'
+}
+
+
 
 
 
@@ -19,6 +25,10 @@ external_css = ["https://codepen.io/anon/pen/mardKv.css"]
 app = dash.Dash(__name__,external_stylesheets=external_css)
 server = app.server
 app.title = 'HCIL Deck'
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
 
 
 theme =  {
